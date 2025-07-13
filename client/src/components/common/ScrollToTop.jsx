2ddd2c6f17fc492c7238633b0910a4cbab565fea
darkkn1ght <peterandy888@gmail.com>
2025-07-13 16:21:50 +0100

    updates

--- client/src/components/common/ScrollToTop.jsx
+++ client/src/components/common/ScrollToTop.jsx
@@ -1,5 +1,14 @@
-// import React from 'react';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
-const ScrollToTop = () => null;
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 
 export default ScrollToTop;
