87742a12926e17493b652fa17f212ab15d3a79cc
darkkn1ght <peterandy888@gmail.com>
2025-07-16 17:38:31 +0100

    login errors

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -10,7 +10,8 @@ class ErrorResponse extends Error {
 }
 
 // Error handler middleware
-const errorHandler = (err, req, res) => {
+// eslint-disable-next-line no-unused-vars
+const errorHandler = (err, req, res, next) => {
   let error = { ...err };
   error.message = err.message;
 
