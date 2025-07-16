4020132dc007628fa3778b91778f0033234d08c9
darkkn1ght <peterandy888@gmail.com>
2025-07-16 17:50:21 +0100

    backend logs fixed

--- server/models/ActivityLog.js
+++ server/models/ActivityLog.js
@@ -31,6 +31,7 @@ const activityLogSchema = new mongoose.Schema({
       'logout',
       'view',
       'get', // Added get as a valid action
+      'head', // Added head as a valid action
       'approve', 'reject', 'export', 'import',
       'bulk_action', 'settings_change', 'password_change',
       'error', 'other'
